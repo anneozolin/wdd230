f0b0a79f929709012885279bebed1413f067a535
anneozolin <anelisa.ozolin@gmail.com>
2023-02-04 20:23:22 -0300

    W05 changes

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -6,8 +6,46 @@ document.querySelector('#current-year').textContent = currentYear;
 let lastModified = document.lastModified;
 document.getElementById('last-modified').textContent = lastModified;
 
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+    ];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ]
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + "," + " " + d.getDate() + " " + monthName + " " + year;
+
 document.getElementById('date').textContent = fulldate;
+
+const weekDay = date.getDay();
+
+if (weekDay == 1 && weekDay == 2){
+    document.querySelector("#message").style.display = "block";
+    }
+else {
+    document.querySelector("#message").style.display = "none";
+    }
 
 
 function toggleMenu () {
